cf25d544a8fafa44f242b9336b5809b3c10acf3c
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2025-05-15 15:37:26 +0530

    build update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,22 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src'),
     },
     extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  build: {
+    rollupOptions: {
+      // Force Rollup to use the JS implementation
+      context: 'globalThis'
+    },
+    target: 'esnext',
+    minify: 'esbuild'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
   }
 })
